981ba3e441f8247a0e625c7149dbc310bbcc17bf
Ryan <vellabonelloryan@gmail.com>
2023-07-14 08:49:48 +0800

    adds header & footer (static HTML)

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,9 +1,13 @@
 @extends('layouts.base')
 
 @section('body')
+    @include('partials.header')
+
     @yield('content')
     
     @isset($slot)
         {{ $slot }}
     @endisset
+
+    @include('partials.footer')
 @endsection
