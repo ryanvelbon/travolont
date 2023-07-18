326454d87b0babae63ecbf9d09809606761ca093
Ryan <vellabonelloryan@gmail.com>
2023-07-18 19:47:20 +0800

    style: defines component class for select menus

--- resources/views/livewire/profile-edit.blade.php
+++ resources/views/livewire/profile-edit.blade.php
@@ -36,7 +36,11 @@
 
         <div>
             <label for="nationality" class="block text-sm font-medium leading-6 text-gray-900">Nationality</label>
-            <select wire:model.lazy="user.nationality" id="nationality" class="mt-2 block w-full rounded-md border-0 py-1.5 pl-3 pr-10 text-gray-900 ring-1 ring-inset ring-gray-300 focus:ring-2 focus:ring-indigo-600 sm:text-sm sm:leading-6">
+            <select
+                wire:model.lazy="user.nationality"
+                id="nationality"
+                class="form-select mt-2 block w-full"
+            >
                 <option selected>-- select --</option>
                 @foreach($countries as $country)
                     <option value="{{$country->id}}">{{ $country->name }}</option>
@@ -72,7 +76,7 @@
                 <select
                     wire:model.lazy="dob_day"
                     id="dob_day"
-                    class="mt-2 block w-full rounded-md border-0 py-1.5 pl-3 pr-10 text-gray-900 ring-1 ring-inset ring-gray-300 focus:ring-2 focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                    class="form-select mt-2 block w-full"
                 >
                     <option selected>Day</option>
                     @for($i=1; $i<=31; $i++)
@@ -81,7 +85,7 @@
                 </select>
                 <select
                     wire:model.lazy="dob_month"
-                    class="mt-2 block w-full rounded-md border-0 py-1.5 pl-3 pr-10 text-gray-900 ring-1 ring-inset ring-gray-300 focus:ring-2 focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                    class="form-select mt-2 block w-full"
                 >
                     <option selected value="">Month</option>
                     @foreach(range(1, 12) as $month)
@@ -90,7 +94,7 @@
                 </select>
                 <select
                     wire:model.lazy="dob_year"
-                    class="mt-2 block w-full rounded-md border-0 py-1.5 pl-3 pr-10 text-gray-900 ring-1 ring-inset ring-gray-300 focus:ring-2 focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                    class="form-select mt-2 block w-full"
                 >
                     <option selected>Year</option>
                     @for($i=2010; $i>=1900; $i--)
