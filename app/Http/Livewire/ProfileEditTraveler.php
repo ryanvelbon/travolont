98328bec1709a75ed7be320db274ea20ce3653cc
Ryan <vellabonelloryan@gmail.com>
2023-07-19 09:24:20 +0800

    adds a template multi-step form Livewire component
    Currently with some dummy fields. Needs to be customized with actual fields.

--- app/Http/Livewire/ProfileEditTraveler.php
+++ app/Http/Livewire/ProfileEditTraveler.php
@@ -3,11 +3,67 @@
 namespace App\Http\Livewire;
 
 use Livewire\Component;
+use App\Models\Traveler;
 
 class ProfileEditTraveler extends Component
 {
+    public $currentStep = 1;
+
+    // Form Fields
+    public $foo;
+    public $goo;
+    public $zoo;
+
     public function render()
     {
         return view('livewire.profile-edit-traveler')->extends('layouts.auth', ['showNavbar' => true]);
+    }
+
+    public function updated($propertyName)
+    {
+        $this->validateOnly($propertyName, $this->stepValidationRules());
+    }
+
+    public function nextStep()
+    {
+        $this->validate($this->stepValidationRules());
+        $this->currentStep++;
+    }
+
+    public function submitForm()
+    {
+        if ($this->currentStep === 3) {
+            $this->save();
+        } else {
+            $this->nextStep();
+        }
+    }
+
+    public function save()
+    {
+        $this->validate($this->stepValidationRules());
+
+        // Add your save logic here, for example
+        Traveler::create([
+            'foo' => $this->foo,
+            'goo' => $this->goo,
+            'zoo' => $this->zoo,
+        ]);
+
+        // Reset fields and step for next use
+        $this->reset('foo', 'goo', 'zoo', 'currentStep');
+
+        session()->flash('message', 'Profile successfully updated.');
+    }
+
+    protected function stepValidationRules()
+    {
+        $rules = [
+            1 => ['foo' => 'required|string|max:255'],
+            2 => ['goo' => 'required|string|max:255'],
+            3 => ['zoo' => 'required|string|max:255'],
+        ];
+
+        return $rules[$this->currentStep];
     }
 }
--- resources/views/livewire/profile-edit-traveler.blade.php
+++ resources/views/livewire/profile-edit-traveler.blade.php
@@ -1,3 +1,46 @@
-<div class="bg-blue-200">
-    Edit traveler profile
+<div class="mx-auto w-full md:w-[600px] px-4 py-4 sm:px-6 lg:px-8 bg-gray-100">
+    @if (session()->has('message'))
+        <div class="alert alert-success">
+            {{ session('message') }}
+        </div>
+    @endif
+
+    <form
+        wire:submit.prevent="submitForm()"
+        class="flex flex-col gap-y-8"
+    >
+
+        @if ($currentStep === 1)
+            <h1>Step 1</h1>
+            <div>
+                <label class="block">Foo</label>
+                <textarea rows="6" class="block w-full" wire:model="foo"></textarea>
+                @error('foo') <p class="input-error-msg">{{ $message }}</p> @enderror
+            </div>
+
+        @elseif ($currentStep === 2)
+            <h1>Step 2</h1>
+            <div>
+                <label class="block">Goo</label>
+                <textarea rows="6" class="block w-full" wire:model="goo"></textarea>
+                @error('goo') <p class="input-error-msg">{{ $message }}</p> @enderror
+            </div>
+
+        @elseif ($currentStep === 3)
+            <h1>Step 3</h1>
+            <div>
+                <label class="block">Zoo</label>
+                <textarea rows="6" class="block w-full" wire:model="zoo"></textarea>
+                @error('zoo') <p class="input-error-msg">{{ $message }}</p> @enderror
+            </div>
+        @endif
+
+        <div>
+            @if ($currentStep === 3)
+                <button class="btn btn-primary" type="submit">Save</button>
+            @else
+                <button class="btn btn-primary-outline" type="submit">Next</button>
+            @endif
+        </div>
+    </form>
 </div>
