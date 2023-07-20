1b79c57d35f6d3d0529500af8284104f69a72ba0
Ryan <vellabonelloryan@gmail.com>
2023-07-20 13:59:30 +0800

    mobile filter sections can be expanded/collapsed

--- resources/views/livewire/travelers-index.blade.php
+++ resources/views/livewire/travelers-index.blade.php
@@ -45,24 +45,19 @@
           <!-- Filters -->
           <form class="mt-4">
             <div class="border-t border-gray-200 pb-4 pt-4">
-              <fieldset>
+              <fieldset x-data="{ open: true }">
                 <legend class="w-full px-2">
                   <!-- Expand/collapse section button -->
-                  <button type="button" class="flex w-full items-center justify-between p-2 text-gray-400 hover:text-gray-500" aria-controls="filter-section-0" aria-expanded="false">
+                  <button @click="open = !open" type="button" class="flex w-full items-center justify-between p-2 text-gray-400 hover:text-gray-500" aria-controls="filter-section-0" aria-expanded="false">
                     <span class="text-sm font-medium text-gray-900">Color</span>
-                    <span class="ml-6 flex h-7 items-center bg-red-600">
-                      <!--
-                        Expand/collapse icon, toggle classes based on section open state.
-
-                        Open: "-rotate-180", Closed: "rotate-0"
-                      -->
-                      <svg class="rotate-0 h-5 w-5 transform" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
+                    <span class="ml-6 flex h-7 items-center">
+                      <svg :class="{'-rotate-180': open, 'rotate-0': !open}" class="h-5 w-5 transform" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
                         <path fill-rule="evenodd" d="M5.23 7.21a.75.75 0 011.06.02L10 11.168l3.71-3.938a.75.75 0 111.08 1.04l-4.25 4.5a.75.75 0 01-1.08 0l-4.25-4.5a.75.75 0 01.02-1.06z" clip-rule="evenodd" />
                       </svg>
                     </span>
                   </button>
                 </legend>
-                <div class="px-4 pb-2 pt-4" id="filter-section-0">
+                <div x-show="open" class="px-4 pb-2 pt-4" id="filter-section-0">
                   <div class="space-y-6">
                     <div class="flex items-center">
                       <input id="color-0-mobile" name="color[]" value="white" type="checkbox" class="h-4 w-4 rounded border-gray-300 text-indigo-600 focus:ring-indigo-500">
@@ -93,24 +88,19 @@
               </fieldset>
             </div>
             <div class="border-t border-gray-200 pb-4 pt-4">
-              <fieldset>
+              <fieldset x-data="{ open: false }">
                 <legend class="w-full px-2">
                   <!-- Expand/collapse section button -->
-                  <button type="button" class="flex w-full items-center justify-between p-2 text-gray-400 hover:text-gray-500" aria-controls="filter-section-1" aria-expanded="false">
+                  <button @click="open = !open" type="button" class="flex w-full items-center justify-between p-2 text-gray-400 hover:text-gray-500" aria-controls="filter-section-1" aria-expanded="false">
                     <span class="text-sm font-medium text-gray-900">Category</span>
                     <span class="ml-6 flex h-7 items-center">
-                      <!--
-                        Expand/collapse icon, toggle classes based on section open state.
-
-                        Open: "-rotate-180", Closed: "rotate-0"
-                      -->
-                      <svg class="rotate-0 h-5 w-5 transform" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
+                      <svg :class="{'-rotate-180': open, 'rotate-0': !open}" class="h-5 w-5 transform" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
                         <path fill-rule="evenodd" d="M5.23 7.21a.75.75 0 011.06.02L10 11.168l3.71-3.938a.75.75 0 111.08 1.04l-4.25 4.5a.75.75 0 01-1.08 0l-4.25-4.5a.75.75 0 01.02-1.06z" clip-rule="evenodd" />
                       </svg>
                     </span>
                   </button>
                 </legend>
-                <div class="px-4 pb-2 pt-4" id="filter-section-1">
+                <div x-show="open" class="px-4 pb-2 pt-4" id="filter-section-1">
                   <div class="space-y-6">
                     <div class="flex items-center">
                       <input id="category-0-mobile" name="category[]" value="new-arrivals" type="checkbox" class="h-4 w-4 rounded border-gray-300 text-indigo-600 focus:ring-indigo-500">
@@ -137,24 +127,19 @@
               </fieldset>
             </div>
             <div class="border-t border-gray-200 pb-4 pt-4">
-              <fieldset>
+              <fieldset x-data="{ open: false }">
                 <legend class="w-full px-2">
                   <!-- Expand/collapse section button -->
-                  <button type="button" class="flex w-full items-center justify-between p-2 text-gray-400 hover:text-gray-500" aria-controls="filter-section-2" aria-expanded="false">
+                  <button @click="open = !open" type="button" class="flex w-full items-center justify-between p-2 text-gray-400 hover:text-gray-500" aria-controls="filter-section-2" aria-expanded="false">
                     <span class="text-sm font-medium text-gray-900">Sizes</span>
                     <span class="ml-6 flex h-7 items-center">
-                      <!--
-                        Expand/collapse icon, toggle classes based on section open state.
-
-                        Open: "-rotate-180", Closed: "rotate-0"
-                      -->
-                      <svg class="rotate-0 h-5 w-5 transform" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
+                      <svg :class="{'-rotate-180': open, 'rotate-0': !open}" class="h-5 w-5 transform" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
                         <path fill-rule="evenodd" d="M5.23 7.21a.75.75 0 011.06.02L10 11.168l3.71-3.938a.75.75 0 111.08 1.04l-4.25 4.5a.75.75 0 01-1.08 0l-4.25-4.5a.75.75 0 01.02-1.06z" clip-rule="evenodd" />
                       </svg>
                     </span>
                   </button>
                 </legend>
-                <div class="px-4 pb-2 pt-4" id="filter-section-2">
+                <div x-show="open" class="px-4 pb-2 pt-4" id="filter-section-2">
                   <div class="space-y-6">
                     <div class="flex items-center">
                       <input id="sizes-0-mobile" name="sizes[]" value="xs" type="checkbox" class="h-4 w-4 rounded border-gray-300 text-indigo-600 focus:ring-indigo-500">
