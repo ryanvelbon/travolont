d6fd361ef325b332e0adbcf4886f0682d5ebad51
Ryan <vellabonelloryan@gmail.com>
2023-07-22 06:58:42 +0800

    guarantees a unique username per run of the User factory

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -12,7 +12,7 @@ class UserFactory extends Factory
     {
         return [
             'account_type' => 'traveler',
-            'username' => fake()->username(),
+            'username' => fake()->unique()->username(),
             'email' => fake()->unique()->safeEmail(),
             'email_verified_at' => now(),
             'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
