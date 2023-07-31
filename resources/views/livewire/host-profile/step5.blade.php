837350ee0f7efa1477eebba2cdeacf242153121c
Ryan <vellabonelloryan@gmail.com>
2023-07-31 19:04:05 +0800

    style: restyles checkbox elements
    & adds Font Awesome icons

--- resources/views/livewire/host-profile/step5.blade.php
+++ resources/views/livewire/host-profile/step5.blade.php
@@ -1,17 +1,26 @@
 <h2 class="text-xl text-gray-800">What do you need help with?</h2>
 <p class="text-sm text-gray-600">Select at least 5</p>
-<div class="flex flex-wrap justify-center items-center gap-3 p-4">
+<div
+    x-data="{ checked: {{ json_encode($selectedServices) }} }"
+    class="flex flex-wrap justify-center items-center gap-3 p-4"
+>
     @foreach($services as $service)
-        <label class="block bg-gray-200 px-5 py-2 rounded-full">
+        <label
+            class="block bg-gray-200 px-5 py-2 rounded-full cursor-pointer hover:shadow-md"
+            :class="{ 'bg-gray-800 text-white': checked.includes({{ $service->id }}) }"
+        >
             <input
+                @click="checked.includes({{ $service->id }}) ? checked = checked.filter(i => i !== {{ $service->id }}) : checked.push({{ $service->id }})"
                 wire:model="selectedServices"
                 type="checkbox"
                 value="{{ $service->id }}"
+                class="hidden"
             >
+            <i class="fa-light {{ $service->icon }} mr-2"></i>
             {{ $service->title }}
         </label>
     @endforeach
 </div>
 @error('selectedServices')
-    <p class="input-error-msg">{{ $message }}</p>
+    <p class="input-error-msg mb-4">{{ $message }}</p>
 @endError
