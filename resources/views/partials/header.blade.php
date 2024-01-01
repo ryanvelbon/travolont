472efd6d77eea78b66595d0b5816720a037cb0df
Ryan <vellabonelloryan@gmail.com>
2024-01-01 17:40:09 +0700

    replaces FontAwesome icons with emojis

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -1,4 +1,4 @@
-<header x-data="{ open: false, showSearch: false }" class="bg-primary-500 shadow">
+<header x-data="{ open: false, showSearch: false }" class="bg-primary-700 shadow">
   <div class="px-2 sm:px-4 lg:px-16">
     <div class="relative flex h-16 justify-between">
       <div class="relative z-10 flex px-2 lg:px-0">
@@ -39,16 +39,16 @@
       <div class="hidden lg:relative lg:z-10 lg:ml-4 lg:flex lg:items-center lg:gap-x-1">
         @auth
           <a href="#" class="flex flex-col justify-center items-center p-2 hover:bg-white/10 w-20">
-            <svg xmlns="http://www.w3.org/2000/svg" height="1.5em" fill="#fff" viewBox="0 0 576 512"><!--! Font Awesome Pro 6.4.0 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2023 Fonticons, Inc. --><path d="M298.6 4c-6-5.3-15.1-5.3-21.2 0L5.4 244c-6.6 5.8-7.3 16-1.4 22.6s16 7.3 22.6 1.4L64 235V432c0 44.2 35.8 80 80 80H432c44.2 0 80-35.8 80-80V235l37.4 33c6.6 5.8 16.7 5.2 22.6-1.4s5.2-16.7-1.4-22.6L298.6 4zM96 432V206.7L288 37.3 480 206.7V432c0 26.5-21.5 48-48 48H368V320c0-17.7-14.3-32-32-32H240c-17.7 0-32 14.3-32 32V480H144c-26.5 0-48-21.5-48-48zm144 48V320h96V480H240z"/></svg>
-            <span class="text-gray-100 text-xs pt-2">Dashboard</span>
+            <span class="text-3xl">🏡</span>
+            <span class="text-gray-100 text-xs">Dashboard</span>
           </a>
           <div x-data="{ open: false }" class="relative">
             <button
               @click="open = !open"
               class="flex flex-col justify-center items-center p-2 hover:bg-white/10 w-20"
             >
-              <svg xmlns="http://www.w3.org/2000/svg" height="1.5em" fill="#fff" viewBox="0 0 448 512"><!--! Font Awesome Pro 6.4.0 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2023 Fonticons, Inc. --><path d="M112 0c8.8 0 16 7.2 16 16V64H320V16c0-8.8 7.2-16 16-16s16 7.2 16 16V64h32c35.3 0 64 28.7 64 64v32 32V448c0 35.3-28.7 64-64 64H64c-35.3 0-64-28.7-64-64V192 160 128C0 92.7 28.7 64 64 64H96V16c0-8.8 7.2-16 16-16zM416 192H312v72H416V192zm0 104H312v80H416V296zm0 112H312v72h72c17.7 0 32-14.3 32-32V408zM280 376V296H168v80H280zM168 408v72H280V408H168zm-32-32V296H32v80H136zM32 408v40c0 17.7 14.3 32 32 32h72V408H32zm0-144H136V192H32v72zm136 0H280V192H168v72zM384 96H64c-17.7 0-32 14.3-32 32v32H416V128c0-17.7-14.3-32-32-32z"/></svg>
-              <span class="text-gray-100 text-xs pt-2">Calendar</span>
+              <span class="text-3xl">🗓</span>
+              <span class="text-gray-100 text-xs">Calendar</span>
             </button>
 
             <!-- Dropdown menu, show/hide based on menu state -->
@@ -79,12 +79,12 @@
             </div>
           </div>
           <a href="#" class="flex flex-col justify-center items-center p-2 hover:bg-white/10 w-20">
-            <svg xmlns="http://www.w3.org/2000/svg" height="1.5em" fill="#fff" viewBox="0 0 512 512"><!--! Font Awesome Pro 6.4.0 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2023 Fonticons, Inc. --><path d="M64 96c-17.7 0-32 14.3-32 32v39.9L227.6 311.3c16.9 12.4 39.9 12.4 56.8 0L480 167.9V128c0-17.7-14.3-32-32-32H64zM32 207.6V384c0 17.7 14.3 32 32 32H448c17.7 0 32-14.3 32-32V207.6L303.3 337.1c-28.2 20.6-66.5 20.6-94.6 0L32 207.6zM0 128C0 92.7 28.7 64 64 64H448c35.3 0 64 28.7 64 64V384c0 35.3-28.7 64-64 64H64c-35.3 0-64-28.7-64-64V128z"/></svg>
-            <span class="text-gray-100 text-xs pt-2">Inbox</span>
+            <span class="text-3xl">✉</span>
+            <span class="text-gray-100 text-xs">Inbox</span>
           </a>
           <button type="button" class="flex flex-col justify-center items-center p-2 hover:bg-white/10 w-20">
-            <svg xmlns="http://www.w3.org/2000/svg" height="1.5em" fill="#fff" viewBox="0 0 448 512"><!--! Font Awesome Pro 6.4.0 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2023 Fonticons, Inc. --><path d="M208 16c0-8.8 7.2-16 16-16s16 7.2 16 16V32.8c80.9 8 144 76.2 144 159.2v29.1c0 43.7 17.4 85.6 48.3 116.6l2.8 2.8c8.3 8.3 13 19.6 13 31.3c0 24.5-19.8 44.3-44.3 44.3H44.3C19.8 416 0 396.2 0 371.7c0-11.7 4.7-23 13-31.3l2.8-2.8C46.6 306.7 64 264.8 64 221.1V192c0-83 63.1-151.2 144-159.2V16zm16 48C153.3 64 96 121.3 96 192v29.1c0 52.2-20.7 102.3-57.7 139.2L35.6 363c-2.3 2.3-3.6 5.4-3.6 8.7c0 6.8 5.5 12.3 12.3 12.3H403.7c6.8 0 12.3-5.5 12.3-12.3c0-3.3-1.3-6.4-3.6-8.7l-2.8-2.8c-36.9-36.9-57.7-87-57.7-139.2V192c0-70.7-57.3-128-128-128zM193.8 458.7c4.4 12.4 16.3 21.3 30.2 21.3s25.8-8.9 30.2-21.3c2.9-8.3 12.1-12.7 20.4-9.8s12.7 12.1 9.8 20.4C275.6 494.2 251.9 512 224 512s-51.6-17.8-60.4-42.7c-2.9-8.3 1.4-17.5 9.8-20.4s17.5 1.4 20.4 9.8z"/></svg>
-            <span class="text-gray-100 text-xs pt-2">Notifications</span>
+            <span class="text-3xl">🔔</span>
+            <span class="text-gray-100 text-xs">Notifications</span>
           </button>
 
           <!-- Profile dropdown -->
