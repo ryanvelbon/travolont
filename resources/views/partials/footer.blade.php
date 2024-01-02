94ca22776ba5993438d02b4f8cd460388fb46ab3
Ryan <vellabonelloryan@gmail.com>
2024-01-02 12:17:13 +0700

    style: applies brand color to footer

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -1,112 +1,112 @@
-<footer class="bg-white" aria-labelledby="footer-heading">
-  <h2 id="footer-heading" class="sr-only">Footer</h2>
-  <div class="mx-auto max-w-7xl px-6 pb-8 pt-20 sm:pt-24 lg:px-8 lg:pt-32">
-    <div class="xl:grid xl:grid-cols-3 xl:gap-8">
-      <div class="grid grid-cols-2 gap-8 xl:col-span-2">
-        <div class="md:grid md:grid-cols-2 md:gap-8">
-          <div>
-            <h3 class="text-sm font-semibold font-serif leading-6 text-gray-900">Solutions</h3>
-            <ul role="list" class="mt-6 space-y-4">
-              <li>
-                <a href="#" class="text-sm leading-6 text-gray-600 hover:text-gray-900">Marketing</a>
-              </li>
-              <li>
-                <a href="#" class="text-sm leading-6 text-gray-600 hover:text-gray-900">Analytics</a>
-              </li>
-              <li>
-                <a href="#" class="text-sm leading-6 text-gray-600 hover:text-gray-900">Commerce</a>
-              </li>
-              <li>
-                <a href="#" class="text-sm leading-6 text-gray-600 hover:text-gray-900">Insights</a>
-              </li>
-            </ul>
-          </div>
-          <div class="mt-10 md:mt-0">
-            <h3 class="text-sm font-semibold font-serif leading-6 text-gray-900">Support</h3>
-            <ul role="list" class="mt-6 space-y-4">
-              <li>
-                <a href="#" class="text-sm leading-6 text-gray-600 hover:text-gray-900">Pricing</a>
-              </li>
-              <li>
-                <a href="#" class="text-sm leading-6 text-gray-600 hover:text-gray-900">Documentation</a>
-              </li>
-              <li>
-                <a href="#" class="text-sm leading-6 text-gray-600 hover:text-gray-900">Guides</a>
-              </li>
-              <li>
-                <a href="#" class="text-sm leading-6 text-gray-600 hover:text-gray-900">API Status</a>
-              </li>
-            </ul>
-          </div>
+<footer class="bg-primary-900" aria-labelledby="footer-heading">
+    <h2 id="footer-heading" class="sr-only">Footer</h2>
+    <div class="mx-auto max-w-7xl px-6 pb-8 pt-20 sm:pt-24 lg:px-8 lg:pt-32">
+        <div class="xl:grid xl:grid-cols-3 xl:gap-8">
+            <div class="grid grid-cols-2 gap-8 xl:col-span-2">
+                <div class="md:grid md:grid-cols-2 md:gap-8">
+                    <div>
+                        <h3 class="text-xl font-semibold font-serif leading-6 text-white">Solutions</h3>
+                        <ul role="list" class="mt-6 space-y-4">
+                            <li>
+                                <a href="#" class="text-sm leading-6 text-primary-200 hover:text-white">Marketing</a>
+                            </li>
+                            <li>
+                                <a href="#" class="text-sm leading-6 text-primary-200 hover:text-white">Analytics</a>
+                            </li>
+                            <li>
+                                <a href="#" class="text-sm leading-6 text-primary-200 hover:text-white">Commerce</a>
+                            </li>
+                            <li>
+                                <a href="#" class="text-sm leading-6 text-primary-200 hover:text-white">Insights</a>
+                            </li>
+                        </ul>
+                    </div>
+                    <div class="mt-10 md:mt-0">
+                        <h3 class="text-xl font-semibold font-serif leading-6 text-white">Support</h3>
+                        <ul role="list" class="mt-6 space-y-4">
+                            <li>
+                                <a href="#" class="text-sm leading-6 text-primary-200 hover:text-white">Pricing</a>
+                            </li>
+                            <li>
+                                <a href="#" class="text-sm leading-6 text-primary-200 hover:text-white">Documentation</a>
+                            </li>
+                            <li>
+                                <a href="#" class="text-sm leading-6 text-primary-200 hover:text-white">Guides</a>
+                            </li>
+                            <li>
+                                <a href="#" class="text-sm leading-6 text-primary-200 hover:text-white">API Status</a>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+                <div class="md:grid md:grid-cols-2 md:gap-8">
+                    <div>
+                        <h3 class="text-xl font-semibold font-serif leading-6 text-white">Company</h3>
+                        <ul role="list" class="mt-6 space-y-4">
+                            <li>
+                                <a href="#" class="text-sm leading-6 text-primary-200 hover:text-white">About</a>
+                            </li>
+                            <li>
+                                <a href="#" class="text-sm leading-6 text-primary-200 hover:text-white">Blog</a>
+                            </li>
+                            <li>
+                                <a href="#" class="text-sm leading-6 text-primary-200 hover:text-white">Jobs</a>
+                            </li>
+                            <li>
+                                <a href="#" class="text-sm leading-6 text-primary-200 hover:text-white">Press</a>
+                            </li>
+                            <li>
+                                <a href="#" class="text-sm leading-6 text-primary-200 hover:text-white">Partners</a>
+                            </li>
+                        </ul>
+                    </div>
+                    <div class="mt-10 md:mt-0">
+                        <h3 class="text-xl font-semibold font-serif leading-6 text-white">Legal</h3>
+                        <ul role="list" class="mt-6 space-y-4">
+                            <li>
+                                <a href="#" class="text-sm leading-6 text-primary-200 hover:text-white">Claim</a>
+                            </li>
+                            <li>
+                                <a href="#" class="text-sm leading-6 text-primary-200 hover:text-white">Privacy</a>
+                            </li>
+                            <li>
+                                <a href="#" class="text-sm leading-6 text-primary-200 hover:text-white">Terms</a>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+            <div class="mt-10 xl:mt-0">
+                <h3 class="text-xl font-semibold font-serif leading-6 text-white">Subscribe to our newsletter</h3>
+                <p class="mt-2 text-sm leading-6 text-primary-200">The latest news, articles, and resources, sent to your inbox weekly.</p>
+                <form class="mt-6 sm:flex sm:max-w-md">
+                    <label for="email-address" class="sr-only">Email address</label>
+                    <input type="email" name="email-address" id="email-address" autocomplete="email" required class="w-full min-w-0 appearance-none rounded-md border-2 bg-white px-3 py-1.5 text-base text-gray-800 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-primary-600 sm:w-64 sm:text-sm sm:leading-6 xl:w-full" placeholder="Enter your email">
+                    <div class="mt-4 sm:ml-4 sm:mt-0 sm:flex-shrink-0">
+                        <button type="submit" class="flex w-full items-center justify-center btn btn-secondary">Subscribe</button>
+                    </div>
+                </form>
+            </div>
         </div>
-        <div class="md:grid md:grid-cols-2 md:gap-8">
-          <div>
-            <h3 class="text-sm font-semibold font-serif leading-6 text-gray-900">Company</h3>
-            <ul role="list" class="mt-6 space-y-4">
-              <li>
-                <a href="#" class="text-sm leading-6 text-gray-600 hover:text-gray-900">About</a>
-              </li>
-              <li>
-                <a href="#" class="text-sm leading-6 text-gray-600 hover:text-gray-900">Blog</a>
-              </li>
-              <li>
-                <a href="#" class="text-sm leading-6 text-gray-600 hover:text-gray-900">Jobs</a>
-              </li>
-              <li>
-                <a href="#" class="text-sm leading-6 text-gray-600 hover:text-gray-900">Press</a>
-              </li>
-              <li>
-                <a href="#" class="text-sm leading-6 text-gray-600 hover:text-gray-900">Partners</a>
-              </li>
-            </ul>
-          </div>
-          <div class="mt-10 md:mt-0">
-            <h3 class="text-sm font-semibold font-serif leading-6 text-gray-900">Legal</h3>
-            <ul role="list" class="mt-6 space-y-4">
-              <li>
-                <a href="#" class="text-sm leading-6 text-gray-600 hover:text-gray-900">Claim</a>
-              </li>
-              <li>
-                <a href="#" class="text-sm leading-6 text-gray-600 hover:text-gray-900">Privacy</a>
-              </li>
-              <li>
-                <a href="#" class="text-sm leading-6 text-gray-600 hover:text-gray-900">Terms</a>
-              </li>
-            </ul>
-          </div>
+        <div class="mt-16 border-t border-secondary-100 pt-8 sm:mt-20 md:flex md:items-center md:justify-between lg:mt-24">
+            <div class="flex space-x-5 md:order-2 text-yellow-100">
+                <a href="https://www.instagram.com/travolont/">
+                    <i class="fa-brands fa-instagram fa-2xl hover:text-primary-400"></i>
+                </a>
+                <a href="https://www.tiktok.com/@travolont">
+                    <i class="fa-brands fa-tiktok fa-2xl hover:text-primary-400"></i>
+                </a>
+                <a href="https://twitter.com/travolont">
+                    <i class="fa-brands fa-twitter fa-2xl hover:text-primary-400"></i>
+                </a>
+                <a href="https://www.reddit.com/r/travolont/">
+                    <i class="fa-brands fa-reddit fa-2xl hover:text-primary-400"></i>
+                </a>
+                <a href="https://id.pinterest.com/travolont/">
+                    <i class="fa-brands fa-pinterest fa-2xl hover:text-primary-400"></i>
+                </a>
+            </div>
+            <p class="mt-8 text-sm leading-5 text-secondary-100 md:order-1 md:mt-0">&copy; 2020 Your Company, Inc. All rights reserved.</p>
         </div>
-      </div>
-      <div class="mt-10 xl:mt-0">
-        <h3 class="text-sm font-semibold font-serif leading-6 text-gray-900">Subscribe to our newsletter</h3>
-        <p class="mt-2 text-sm leading-6 text-gray-600">The latest news, articles, and resources, sent to your inbox weekly.</p>
-        <form class="mt-6 sm:flex sm:max-w-md">
-          <label for="email-address" class="sr-only">Email address</label>
-          <input type="email" name="email-address" id="email-address" autocomplete="email" required class="w-full min-w-0 appearance-none rounded-md border-2 bg-white px-3 py-1.5 text-base text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-primary-600 sm:w-64 sm:text-sm sm:leading-6 xl:w-full" placeholder="Enter your email">
-          <div class="mt-4 sm:ml-4 sm:mt-0 sm:flex-shrink-0">
-            <button type="submit" class="flex w-full items-center justify-center btn btn-primary">Subscribe</button>
-          </div>
-        </form>
-      </div>
     </div>
-    <div class="mt-16 border-t border-gray-900/10 pt-8 sm:mt-20 md:flex md:items-center md:justify-between lg:mt-24">
-      <div class="flex space-x-5 md:order-2">
-        <a href="https://www.instagram.com/travolont/">
-          <i class="fa-brands fa-instagram fa-2xl hover:text-primary-600"></i>
-        </a>
-        <a href="https://www.tiktok.com/@travolont">
-          <i class="fa-brands fa-tiktok fa-2xl hover:text-primary-600"></i>
-        </a>
-        <a href="https://twitter.com/travolont">
-          <i class="fa-brands fa-twitter fa-2xl hover:text-primary-600"></i>
-        </a>
-        <a href="https://www.reddit.com/r/travolont/">
-          <i class="fa-brands fa-reddit fa-2xl hover:text-primary-600"></i>
-        </a>
-        <a href="https://id.pinterest.com/travolont/">
-          <i class="fa-brands fa-pinterest fa-2xl hover:text-primary-600"></i>
-        </a>
-      </div>
-      <p class="mt-8 text-xs leading-5 text-gray-500 md:order-1 md:mt-0">&copy; 2020 Your Company, Inc. All rights reserved.</p>
-    </div>
-  </div>
 </footer>
