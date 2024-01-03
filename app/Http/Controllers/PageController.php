3811aa06f823f155b8f418c18e25658eac1e20af
Ryan <vellabonelloryan@gmail.com>
2024-01-03 15:21:09 +0700

    caches database queries

--- app/Http/Controllers/PageController.php
+++ app/Http/Controllers/PageController.php
@@ -5,18 +5,23 @@ namespace App\Http\Controllers;
 use App\Models\Country;
 use App\Models\HostType;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Cache;
 
 class PageController extends Controller
 {
     public function home()
     {
-        $categories = HostType::all();
+        $categories = Cache::rememberForever('host_types', function () {
+            return HostType::all();
+        });
 
-        $featCountries = Country::whereNotNull('order')
-            ->withCount('hosts')
-            ->withCount('travelers')
-            ->orderBy('order')
-            ->get();
+        $featCountries = Cache::remember('feat_countries', 60*60*24, function () {
+            return Country::whereNotNull('order')
+                ->withCount('hosts')
+                ->withCount('travelers')
+                ->orderBy('hosts_count', 'desc')
+                ->get();
+        });
 
         return view('welcome', [
             'categories' => $categories,
