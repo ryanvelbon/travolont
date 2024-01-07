cd70286769ef846755f1efd7546985ee27c9148f
Ryan <vellabonelloryan@gmail.com>
2024-01-07 11:43:51 +0700

    style: wraps checkbox HTML inside <label> tags

--- resources/views/livewire/profile/host/index.blade.php
+++ resources/views/livewire/profile/host/index.blade.php
@@ -104,18 +104,18 @@
                             @livewire('city-combobox')
                             <fieldset class="mt-6">
                                 <legend class="block text-sm font-medium text-gray-900">Categories</legend>
-                                <div class="space-y-3 pt-6">
+                                <div class="flex flex-wrap gap-2 pt-6">
                                     @foreach($hostTypes as $hostType)
-                                        <div class="flex items-center">
+                                        <label
+                                            wire:key="{{ $hostType->id }}"
+                                            class="text-sm text-gray-600 px-4 py-2 rounded-full cursor-pointer select-none border border-gray-300"
+                                        >
                                             <input
-                                                wire:model.live="selectedHostTypes"
                                                 value="{{ $hostType->id }}"
-                                                id="host-type-{{ $hostType->id }}"
                                                 type="checkbox"
-                                                class="h-4 w-4 rounded border-gray-300 text-primary-600 focus:ring-primary-500"
                                             >
-                                            <label for="{{ $hostType->id }}" class="ml-3 text-sm text-gray-600">{{ $hostType->title }}</label>
-                                        </div>
+                                            <span>{{ $hostType->title }}</span>
+                                        </label>
                                     @endforeach
                                 </div>
                             </fieldset>
