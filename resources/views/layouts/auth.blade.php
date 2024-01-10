2ea93c8b52463ea7b7716d8c7b13f7cfec84cde1
Ryan <vellabonelloryan@gmail.com>
2024-01-10 12:04:10 +0700

    style: applies z-index to nav bar
    Making its shadow visible as it was previously covered by the sibling <div>. Also the <div> starts from the top of the viewport.

--- resources/views/layouts/auth.blade.php
+++ resources/views/layouts/auth.blade.php
@@ -1,22 +1,24 @@
 @extends('layouts.base')
 
-@isset($showNavbar)
-<nav class="bg-white shadow-md mb-2">
-    <div class="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">
-        <div class="flex h-12 items-center justify-between">
-            <a href="{{ route('home') }}">home</a>
-            <a href="">something</a>
+<div class="relative">
+    @isset($showNavbar)
+    <nav class="absolute z-10 w-full bg-white shadow-md">
+        <div class="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">
+            <div class="flex h-12 items-center justify-between">
+                <a href="{{ route('home') }}">home</a>
+                <a href="">something</a>
+            </div>
         </div>
-    </div>
-</nav>
-@endisset
+    </nav>
+    @endisset
 
-@section('body')
-    <div class="flex flex-col justify-center min-h-screen py-12 sm:px-6 lg:px-8 {{ $bgColor ?? 'bg-gray-50' }}">
-        @yield('content')
+    @section('body')
+        <div class="flex flex-col justify-center min-h-screen py-12 sm:px-6 lg:px-8 {{ $bgColor ?? 'bg-gray-50' }}">
+            @yield('content')
 
-        @isset($slot)
-            {{ $slot }}
-        @endisset
-    </div>
-@endsection
+            @isset($slot)
+                {{ $slot }}
+            @endisset
+        </div>
+    @endsection
+</div>
